dec47957eb43e5df423fba65cfd48eb5d3b44c31
jinseo-jang <jinseo.jang@gmail.com>
2022-03-10 20:18:44 +0900

    Added Auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import { Amplify, Auth } from 'aws-amplify';
 
-function App() {
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export default function App() {
+  const [userInfo, setUserInfo] = useState({});
+  useEffect(() => {
+       getUserInfo(setUserInfo)
+     },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello SaaS Lens Lab</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {JSON.stringify(userInfo.attributes.email)}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
-export default App;
+async function getUserInfo(setUserInfo) {
+  const userInfo = await Auth.currentUserInfo();
+  console.log('userInfo', userInfo.attributes.email);
+  setUserInfo(userInfo);
+}
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header>
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>We now have Auth!</h1>
+//       </header>
+//       <AmplifySignOut />
+//     </div>
+//   );
+// }
+
+// export default withAuthenticator(App);
